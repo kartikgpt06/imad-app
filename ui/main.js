aa985fcb951cc0655236a613738c5df524722531
kartikgpt06 <kartikgpt06@gmail.com>
2017-08-26 01:20:01 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -9,10 +9,10 @@ element.innerHTML='New value';
 var img= document.getElementById('madi');
 var marginLeft=0; 
 function moveRight() {
-    marginLeft=marginLeft + 5;
+    marginLeft=marginLeft + 50;
     img.style.marginLeft=marginLeft +'px';
 }
 img.onclick=function() {
-    var interval=setInterval(moveRight,50);
+    var interval=setInterval(moveRight,500);
     img.style.marginleft='100px';
 };
