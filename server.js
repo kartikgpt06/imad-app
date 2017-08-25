919010c93ec4f6efe07bb9cd969200c0a7125691
kartikgpt06 <kartikgpt06@gmail.com>
2017-08-26 00:31:08 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -117,7 +117,7 @@ app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
-var port = 8080;
-app.listen(8080, function () {
+var port = 80;
+app.listen(80, function () {
   console.log(`IMAD course app listening on port ${port}!`);
 });
