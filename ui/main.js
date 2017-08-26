d345b2b77e0690027cad446b9e7e5ba07e6e3a46
kartikgpt06 <kartikgpt06@gmail.com>
2017-08-26 22:25:42 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,18 +1,14 @@
-console.log('Loaded!');
+//Counter code
 
-//Change the text of the main-text div
-var element= document.getElementById('main-text'
-            );
-element.innerHTML='New value';
-
-//Move the image
-var img= document.getElementById('madi');
-var marginLeft=0; 
-function moveRight() {
-    marginLeft=marginLeft + 5;
-    img.style.marginLeft=marginLeft +'px';
-}
-img.onclick=function() {
-    var interval=setInterval(moveRight,50);
-    img.style.marginleft='100px';
+var button=document.getElementById('counter');
+button.onclick = function() {
+    
+    // Make a request to the counter endpoint
+    
+    //Capture the response and store it in a variable
+    
+    //Render the variable in the counter span
+    counter = counter + 1;
+    var span = document.getElementById('count');
+    span.innerHTML = counter.toString();
 };
