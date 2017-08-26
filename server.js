7f9180da137dec16e1c924ceab419809acaa49dd
kartikgpt06 <kartikgpt06@gmail.com>
2017-08-26 23:28:30 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -97,9 +97,10 @@ return htmlTemplate;
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+
 var counter=0;
 app.get('/counter', function(req,res) {
-    counter= counter +1;
+    counter = counter +1;
     res.send(counter.toString());
 });
 
@@ -120,12 +121,12 @@ app.get('/:articleName',function(req,res){
     res.send(createTemplate(articles[articleName]));
 });
 
-app.get('/ui/main.js', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
+app.get('/ui/style.css', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
-app.get('/ui/madi.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
+app.get('/ui/main.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
 
 
