478df15a3c8af2589ec56e4e33dcf3de4e9dcca9
kartikgpt06 <kartikgpt06@gmail.com>
2017-09-07 01:17:13 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var morgan = require('morgan');
 var path = require('path');
 var Pool= require('pg').Pool;
 var crypto = require("crypto");
+var bodyParser = require('body-parser');
 
 var config = {
     user: 'kartikgpt06',
@@ -13,7 +14,7 @@ var config = {
 };
 var app = express();
 app.use(morgan('combined'));
-
+app.use(bodyParser.json());
 
 function createTemplate (data) {
     var title=data.title;
@@ -56,7 +57,6 @@ return htmlTemplate;
 }
 
 
-
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
@@ -72,11 +72,26 @@ function hash(input, salt) {
     // 'password' -> 'password-this-is-a-salt' -> <hash> -> <hash> x 10x times.
 }
 
-
-
 app.get("/hash/:input", function(req,res) {
     var hashedString = hash(req.params.input, 'this-is-some-random-string');
     res.send(hashedString);
+});
+
+app.post('/create-user', function (req,res) {
+   //username,password
+   //JSON
+   var username = req.body.username;
+   var password = req.password;
+   var salt = crypto.getRandomBytes(128).toString('hex');
+   var dbString = hash(password, salt);
+   pool.query('INSERT INTO "user" (username, password) VALUES ($1, $2)',[username, dbstring], function(err,result) {
+          if (err) {
+            res.status(500).send(err.toString());
+        } else {
+            res.send('User successfully created: ' + username);
+        }
+       
+   });
 });
 
 var pool = new Pool(config);
