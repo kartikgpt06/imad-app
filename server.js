1b403606aeeac24b08434a76fbdc3012ff81f1cf
kartikgpt06 <kartikgpt06@gmail.com>
2017-09-07 22:01:28 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -85,7 +85,7 @@ app.post('/create-user', function (req,res) {
    var password = req.body.password;
    var salt = crypto.randomBytes(128).toString('hex');
    var dbString = hash(password, salt);
-   Pool.query('INSERT INTO "user" (username, password) VALUES ($1, $2)',[username, dbString], function(err,result) {
+   pool.query('INSERT INTO "user" (username, password) VALUES ($1, $2)',[username, dbString], function(err,result) {
           if (err) {
             res.status(500).send(err.toString());
         } else {
@@ -95,11 +95,37 @@ app.post('/create-user', function (req,res) {
    });
 });
 
-var Pool = new Pool(config);
+app.post('/login', function(req,res){
+    var username = req.body.username;
+   var password = req.body.password;
+    pool.query('SELECT * FROM  "user" username = $1',[username], function(err,result) {
+          if (err) {
+            res.status(500).send(err.toString());
+        } else {
+            if (result.rows.length === 0) {
+                res.send(403).send('username/password is invalid');
+            } else {
+                //Match the password
+                var dbString = result.rows[0].password;
+                var salt = dbString.split('$')[2];
+                var hashedPassword = hash(password, salt); // Creating a hash based on the password submitted and the original salt
+                if (hashedPassword === dbString) {
+                    res.send('credentials correct');
+                } else {
+                    res.send(403).send('username/passwor is invalid');
+                }
+            }
+            
+        }
+       
+   });
+});
+
+var pool = new Pool(config);
 app.get('/test-db',function(req,res){
     //make a select request
     //return a response with the results
-    Pool.query('SELECT * FROM test',function(err,result) {
+    pool.query('SELECT * FROM test',function(err,result) {
         if (err) {
             res.status(500).send(err.toString());
         } else {
