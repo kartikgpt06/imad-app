c99c65c3292a4cb1d7f1a00ff5fd8922ddd4f6bc
kartikgpt06 <kartikgpt06@gmail.com>
2017-09-07 11:59:16 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -95,7 +95,7 @@ app.post('/create-user', function (req,res) {
    });
 });
 
-var Pool = new Pool(config);
+var pool = new Pool(config);
 app.get('/test-db',function(req,res){
     //make a select request
     //return a response with the results
