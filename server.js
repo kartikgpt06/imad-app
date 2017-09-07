db53ad6e55e4238f8712c1b8770510317e7d205f
kartikgpt06 <kartikgpt06@gmail.com>
2017-09-07 20:37:23 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -99,7 +99,7 @@ var Pool = new Pool(config);
 app.get('/test-db',function(req,res){
     //make a select request
     //return a response with the results
-    pool.query('SELECT * FROM test',function(err,result) {
+    Pool.query('SELECT * FROM test',function(err,result) {
         if (err) {
             res.status(500).send(err.toString());
         } else {
@@ -130,7 +130,7 @@ app.get('/articles/:articleName',function(req,res){
     //articles[articleName] == {} content object for article one
     
     //SELECT * FROM article WHERE title = '\';DELETE WHERE a= \'asdf'
-    pool.query("SELECT * FROM article WHERE title= $1" , [req.params.articleName] ,function(err,result) {
+    Pool.query("SELECT * FROM article WHERE title= $1" , [req.params.articleName] ,function(err,result) {
         if (err) {
             res.status(500).send(err.toString());
         } else {
